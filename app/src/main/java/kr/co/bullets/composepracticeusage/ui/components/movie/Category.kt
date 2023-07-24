fbf117791464bedfbf4fe3bd985089403d5b96b9
Bullets-jyr <jyr@bullets.co.kr>
2023-07-25 07:37:28 +0900

    07. 영화 카테고리 만들기

---
+++ app/src/main/java/kr/co/bullets/composepracticeusage/ui/components/movie/Category.kt
@@ -0,0 +1,58 @@
+package kr.co.bullets.composepracticeusage.ui.components.movie
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CategoryRow() {
+    Column {
+        CategoryTitle("Action")
+        LazyRow(
+            contentPadding = PaddingValues(
+                horizontal = 10.dp
+            )
+        ) {
+//            itemsIndexed
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryTitle(s: String) {
+    Text(
+        text = "Action",
+        modifier = Modifier.padding(10.dp)
+    )
+}
+
+@Preview
+@Composable
+fun CategoryRowPreview() {
+    CategoryRow()
+}
--- app/src/main/java/kr/co/bullets/composepracticeusage/ui/components/movie/MovieItem.kt
+++ app/src/main/java/kr/co/bullets/composepracticeusage/ui/components/movie/MovieItem.kt
@@ -3,14 +3,18 @@ package kr.co.bullets.composepracticeusage.ui.components.movie
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.co.bullets.composepracticeusage.R
 
 private val CARD_WIDTH = 150.dp
 
@@ -39,6 +43,16 @@ fun MovieItem() {
             ),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(12.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_rating),
+                tint = Color.Black.copy(
+                    alpha = 0.5f
+                ),
+                contentDescription = "rating icon"
+            )
             Text(text = "9.0")
         }
     }
